e40ca9e1f3cd5bc5ee1b8f4519bbcc7e3ac7e8eb
Khadijah <s22b0008@siswa.umk.edu.my>
2025-11-02 19:07:23 +0800

    Update objective1.py

--- objective1.py
+++ objective1.py
@@ -291,20 +291,11 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.markdown(
-    """
-    <div style="
-        background-color:#BDB5D5;
-        color: #000000; /* Black text color for contrast */;
-        padding:15px 20px;
-        border-radius:10px;
-        border:1px solid #d1d5db;
-    ">
-    is a multidisciplinary field that focuses on transforming complex
-    scientific data into visual forms that are easier to understand, interpret, and communicate.
-    Through the use of computational techniques, visualization helps researchers explore datasets,
-    identify hidden patterns, and gain insights that would otherwise remain obscure in numerical form.
-    </div>
-    """,
-    unsafe_allow_html=True
+st.write(
+  """
+  **Scientific Visualization** is a multidisciplinary field that focuses on transforming complex
+scientific data into visual forms that are easier to understand, interpret, and communicate.
+  Through the use of computational techniques, visualization helps researchers explore datasets,
+identify hidden patterns, and gain insights that would otherwise remain obscure in numerical form.
+  """
 )
